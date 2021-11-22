7cc915a8a120dde36d392c54134f5f84d609355d
incur <wbraschos@gmail.com>
2021-11-22 20:56:20 +0100

    Timeline änderungen

--- devices/heat_exchanger.py
+++ devices/heat_exchanger.py
@@ -2,51 +2,84 @@ import numpy
 import numpy as np
 
 
+def waermeaustausch(m_strom_warm, c_warm, m_strom_kalt, c_kalt,
+                    in_warm, in_kalt, k, flaeche):
+    """
+    :param m_strom_warm: Massestrom Produktseite [Kg*s]
+    :param c_warm: Wärmekapazität Produktseite [J/kgK]
+    :param m_strom_kalt: Massestrom Kühlseite [Kg*s]
+    :param c_kalt: Wärmekapazität Kühlseit [J/kgK]
+    :param in_warm: Eintrittstemperatur Produktseite [K]
+    :param in_kalt: Eintrittstemperatur Kühlseite
+    :param k: Wärmekoeffizent [W/m²k]
+    :param flaeche: Wärmeaustauschfläche [m²]
+    :return: Austrittstemperatur Produktseite [K], Austrittstemperatur Kühlseite [K]
+    """
+    c_strom_warm = m_strom_warm * c_warm
+    c_strom_kalt = m_strom_kalt * c_kalt + 0.0000001
+    ntu_warm = (k * flaeche) / c_strom_warm
+    ntu_kalt = (k * flaeche) / c_strom_kalt
+    w1_w2 = c_strom_warm / c_strom_kalt
+    w2_w1 = c_strom_kalt / c_strom_warm
+
+    wirkung_warm = (1 - np.exp(-ntu_warm * 1 * (1 - w1_w2))) / (1 - w1_w2 * np.exp(-ntu_warm * (1 - w1_w2)))
+    wirkung_kalt = (1 - np.exp(-ntu_kalt * 1 * (1 - w2_w1))) / (1 - w2_w1 * np.exp(-ntu_kalt * (1 - w2_w1)))
+
+    if np.isnan(wirkung_warm) or np.isnan(wirkung_kalt):
+        out_warm = in_warm
+        out_kalt = in_kalt
+    else:
+        out_warm = in_warm - wirkung_warm * (in_warm - in_kalt) * 1
+        out_kalt = in_kalt + (in_warm - in_kalt) * wirkung_kalt * 1
+
+    return {'temp_out_warm': out_warm, 'temp_out_kalt': out_kalt}
+
+
+def massestrom(volumenstrom, dichte):
+    return (dichte * volumenstrom) / 3600
+
+
 class WT:
-    def __init__(self, timeline, dt):
+    def __init__(self, timeline):
         self.flaeche = 5.37  # m²
         self.k = 1200  # W/m²k
 
-        # Fluid Warm
+        # Produktstrom
         self.waermekapazitaet_warm = 4186  # J/kgK
         self.eintritt_warm = 60  # °C
         self.massestrom_warm = (982.3 * 6.13) / 3600
 
-        # Fluid Kalt
+        # Kühlmittelseite
         self.waermekapazitaet_kalt = 4179.8  # J/kgK
         self.eintritt_kalt = 20  # °C
         self.dichte_kalt = 997.2
-        self.massestrom_kalt = self.massestrom(0)
+        self.massestrom_kalt = massestrom(0, self.dichte_kalt)
 
         # Ausgang Fluide
-        self.t_warm = self.eintritt_warm
-        self.t_kalt = self.eintritt_kalt
+        self.temp_now_produkt = self.eintritt_warm
+        self.temp_now_kalt = self.eintritt_kalt
 
         # Simulation
-        self.r_warm = np.zeros_like(timeline)
-        self.r_warm[0] = self.t_warm
-        self.r_kalt = np.zeros_like(timeline)
-        self.r_kalt[0] = self.t_kalt
-
-    def massestrom(self, volumenstrom):
-        return (self.dichte_kalt * volumenstrom) / 3600
+        self.r_warm = np.zeros_like(timeline.t)
+        self.r_warm[0] = self.temp_now_produkt
+        self.r_kalt = np.zeros_like(timeline.t)
+        self.r_kalt[0] = self.temp_now_kalt
 
     def update(self, now, volumenstrom):
-        self.massestrom_kalt = self.massestrom(volumenstrom)
+        self.massestrom_kalt = massestrom(volumenstrom, self.dichte_kalt)
 
-        # Hilfsgrößen
-        waermekapazitaetstrom_warm = self.massestrom_warm * self.waermekapazitaet_warm  # J/K*s
-        waermekapazitaetstrom_kalt = (self.massestrom_kalt * self.waermekapazitaet_kalt) + 0.0000001  # J/K*s
-        ntu_warm = (self.k * self.flaeche) / waermekapazitaetstrom_warm
-        ntu_kalt = (self.k * self.flaeche) / waermekapazitaetstrom_kalt
-        w1_w2 = waermekapazitaetstrom_warm / waermekapazitaetstrom_kalt
-        w2_w1 = waermekapazitaetstrom_kalt / waermekapazitaetstrom_warm
+        t_out = waermeaustausch(self.massestrom_warm, self.waermekapazitaet_warm,
+                                self.massestrom_kalt, self.waermekapazitaet_kalt,
+                                self.eintritt_warm, self.eintritt_kalt, self.k, self.flaeche)
 
-        wirkung_warm = (1 - np.exp(-ntu_warm * 1 * (1 - w1_w2))) / (1 - w1_w2 * np.exp(-ntu_warm * (1 - w1_w2)))
-        wirkung_kalt = (1 - np.exp(-ntu_kalt * 1 * (1 - w2_w1))) / (1 - w2_w1 * np.exp(-ntu_kalt * (1 - w2_w1)))
+        self.r_warm[now] = t_out['temp_out_warm']
+        self.r_kalt[now] = t_out['temp_out_kalt']
 
-        self.t_warm = self.eintritt_warm - wirkung_warm * (self.eintritt_warm - self.eintritt_kalt) * 1
-        self.t_kalt = self.eintritt_kalt + (self.eintritt_warm - self.eintritt_kalt) * wirkung_kalt * 1
+    def get_nowvalues_ma(self):
+        low = self.eintritt_kalt
+        high = self.eintritt_warm
 
-        self.r_warm[now] = self.t_warm
-        self.r_kalt[now] = self.t_kalt
+        kalt_ma = np.interp(self.temp_now_kalt, (low, high), (4, 20))
+        warm_ma = np.interp(self.temp_now_produkt, (low, high), (4, 20))
+
+        return kalt_ma, warm_ma
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from devices.valve import Valve
+from devices.valve import ControlValve
 from devices.heat_exchanger import WT
+# from devices.pid import PID
 
 
 def main():
-    timeline = TimeLine()
-    valve0 = Valve(timeline.t, timeline.dt)
-    wt0 = WT(timeline.t, timeline.dt)
+    timeline = TimeLine(20)
+    valve0 = ControlValve(timeline.dt)
+    wt0 = WT(timeline)
+
+    timeline.add_proc('ventilstellung', 0)
+    timeline.add_proc('volumenstrom', 0)
 
     for now in range(1, len(timeline.t)):
-        prev = now-1
-
-        valve0.update(now)
+        s, v = valve0.update()
         wt0.update(now, valve0.v_now)
+
+        # Update Timeline
+        timeline.write_proc('ventilstellung', now, s)
+        timeline.write_proc('volumenstrom', now, v)
 
     plot_sim(timeline, valve0, wt0)
 
@@ -26,23 +32,23 @@ def plot_sim(timeline, valve0, wt0):
     ax1.set_xlabel('t [s]')
     ax1.set_ylabel('Ventil [%]')
     ax1.grid()
-    ax1.plot(timeline.t, valve0.r)
+    ax1.plot(timeline.t, timeline.get_proc('ventilstellung'))
 
     ax2 = fig.add_subplot(2, 2, 2)
     ax2.set_xlabel('t [s]')
     ax2.set_ylabel('Volumenstrom [m³/h]')
     ax2.grid()
-    ax2.plot(timeline.t, valve0.v)
+    ax2.plot(timeline.t, timeline.get_proc('volumenstrom'))
 
     ax3 = fig.add_subplot(2, 2, 3)
     ax3.set_xlabel('t [s]')
-    ax3.set_ylabel('T_warm [°C]')
+    ax3.set_ylabel('Produkt [°C]')
     ax3.grid()
     ax3.plot(timeline.t, wt0.r_warm)
 
     ax4 = fig.add_subplot(2, 2, 4)
     ax4.set_xlabel('t [s]')
-    ax4.set_ylabel('T_kalt [°C]')
+    ax4.set_ylabel('Kühlwasser [°C]')
     ax4.grid()
     ax4.plot(timeline.t, wt0.r_kalt)
 
@@ -50,12 +56,22 @@ def plot_sim(timeline, valve0, wt0):
 
 
 class TimeLine:
-    def __init__(self):
-        self.t_max = 20  # Simulationsdauer [s]
+    def __init__(self, t_max):
+        self.t_max = t_max  # Simulationsdauer [s]
         self.dt = 0.001  # Zeitschrittweite [s]
         self.t = np.linspace(0, self.t_max, int(self.t_max / self.dt))
+        self.procs = {}
+
+    def add_proc(self, name, startwert):
+        self.procs[name] = np.zeros_like(self.t)
+        self.write_proc(name, 0, startwert)
+
+    def get_proc(self, name):
+        return self.procs[name]
+
+    def write_proc(self, name, now, value):
+        self.procs[name][now] = value
 
 
 if __name__ == '__main__':
-    np.seterr(over='ignore', invalid='ignore')
     main()
